0bd5263315467b8bf5d48ca7b87a23bba7c1dc95
Tharun Vemula <tharunvind@gmail.com>
2023-06-21 11:55:48 +0530

    fork() system call

--- process/sample.3.30.c
+++ process/sample.3.30.c
@@ -3,7 +3,13 @@
 #include<unistd.h>
 
 int value = 5;
-
+/**
+ * The fork function creates a copy of the calling process. 
+ * The only distinction between the two processes is that the value returned to one of them, 
+ * referred to as the parent process, will be the process ID of the copy.
+ * The copy is usually referred to as the child process. The value returned to the child process will be zero.
+ * child receives copy of the parent (code & data)
+*/
 int main(){
     pid_t pid;
     pid = fork();
---
+++ process/sample.3.33.c
@@ -0,0 +1,30 @@
+#include<sys/types.h>
+#include<stdio.h>
+#include<unistd.h>
+
+int value = 5;
+
+int main(){
+    pid_t pid, pid1;
+    pid = fork();
+    printf("%d", pid);
+    if(pid < 0){
+        fprintf(stderr, "Fork Failed");
+        return 1;
+    }
+    else if(pid == 0){ /* Child Process*/
+        pid1 = getpid(); /** Get the Process ID of the calling process*/
+        printf("child: pid = %d\n", pid);
+        printf("child: pid1 = %d\n", pid1);
+        return 0;
+    }
+
+    else if(pid > 0){ /* Parent Process */
+        pid1 = getpid();
+        printf("parent: pid = %d\n", pid);
+        printf("parent: pid1 = %d\n", pid1);
+        wait(NULL);
+        return 0;
+    }
+
+}
